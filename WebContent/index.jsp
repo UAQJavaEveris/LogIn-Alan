b8f03bf77ce9f0d446e05cf61c4bb0258ece7d24
Mango3313 <daft_noob3313@live.com>
2016-12-04 23:26:14 -0600

    Add files via upload

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Forma de acceso</title>
+</head>
+<body>
+<div>
+<h3>LogIn - Alfa</h3> 
+<form method="get" action='Servlet'>
+Nombre: <br>
+<input type="text" name ="User"><br>
+Contraseña: <br>
+<input type="password" name ="Passw"> <br>
+<input type ="submit" name ="Aceptar1"><br>
+</form> <br>
+</div>
+</body>
+</html>
---
+++ src/com/mx/taller/servlets/Servlet.java
@@ -0,0 +1,59 @@
+package com.mx.taller.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<String> users = new ArrayList<String>();
+		List<String> passs = new ArrayList<String>();
+		users.add("Jorge");
+		passs.add("D4ftpunk");
+		String user = request.getParameter("User");
+		String pass = request.getParameter("Passw");
+			if(users.contains(user) && passs.contains(pass)){
+					RequestDispatcher req = request.getRequestDispatcher("Pass.jsp");
+					req.forward(request, response);
+			}else{
+				RequestDispatcher req = request.getRequestDispatcher("Deny.jsp");
+				req.forward(request, response);
+			}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
